68e9faeae9df21a0789354bb5f2322a66c639eac
kimberley-saka <kimberleysaka@gmail.com>
2024-05-02 11:17:18 +0200

    first arduino code

---
+++ sketch_may2a/sketch_may2a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(9, OUTPUT); // pinMode(pinNumber, function);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digitalWrite(9, HIGH); // turn on the LED light, high
+
+}
