05d0e7ffaaa8323e023e746798d8cf7613f8b366
kimberley-saka <kimberleysaka@gmail.com>
2024-05-02 12:02:56 +0200

    updated file

--- sketch_may2a/sketch_may2a.ino
+++ sketch_may2a/sketch_may2a.ino
@@ -1,13 +1,35 @@
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(9, OUTPUT); // pinMode(pinNumber, function);
+  pinMode(10, OUTPUT); // pinMode(pinNumber, function); to select the pin number and as an output
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+  
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(10, HIGH); // turn on the LED light, high
   
-  digitalWrite(9, HIGH); // turn on the LED light, high
+  delay(1000);
+
+  digitalWrite(10, LOW);
+
+  delay(1000);
+
+  digitalWrite(11,  HIGH);
+
+  delay(1000);
+
+  digitalWrite(11, LOW);
+
+  delay(1000);
+
+
+  
 
 }
