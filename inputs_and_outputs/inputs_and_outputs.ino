449ffd9667d818acada592f8dfe29f0ded5f7e66
kimberley-saka <kimberleysaka@gmail.com>
2024-05-03 11:32:44 +0200

    added a comment

--- inputs_and_outputs/inputs_and_outputs.ino
+++ inputs_and_outputs/inputs_and_outputs.ino
@@ -9,7 +9,7 @@ int led1 = 10;
 void setup() {
   pinMode(button, INPUT);
   pinMode(led1, OUTPUT);
-  Serial.begin(9600); // listed for data passing throu a serial bus
+  Serial.begin(9600); // listen for data passing throu a serial bus
 
 }
 
@@ -21,7 +21,7 @@ void loop() {
   delay(200);
 
   if (buttonSwitch == 1) {
-    digitalWrite(led1, HIGH);
+    digitalWrite(led1, HIGH); // switch on led light if button is clicked
   }
 
   else {
