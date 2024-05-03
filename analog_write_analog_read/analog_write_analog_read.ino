c1d2a5ef2fb51d11ab4f55cbba0010325353da90
kimberley-saka <kimberleysaka@gmail.com>
2024-05-03 12:25:40 +0200

    added a comment

--- analog_write_analog_read/analog_write_analog_read.ino
+++ analog_write_analog_read/analog_write_analog_read.ino
@@ -17,16 +17,22 @@ void loop() {
   swt = digitalRead(button);
   
   if(swt == 1) { // if button is pressed
-    delay(500);
+    delay(200);
     
     while(swt == 0) {
       read = analogRead(tune);
       brightness = map(read, 0, 1023, 0, 1023); // map(input, min-input, max-input, min-output, max-output)
       analogWrite(led, brightness);
     }
+
+    digitalWrite(led, LOW);
+    delay(200);
   }
 
   else {
+    read = analogRead(sensor);
+    brightness = map(read, 0, 1023, 0, 1023);
+    analogWrite(led, brightness);
 
   }
 }
