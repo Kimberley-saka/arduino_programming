0c1b804f52d1006179bebeddb2bc59e747438d89
kimberley-saka <kimberleysaka@gmail.com>
2024-05-08 11:15:58 +0200

    working with lcds

--- sensorsContd/sensorsContd.ino
+++ sensorsContd/sensorsContd.ino
@@ -8,9 +8,22 @@ hd44780_I2Cexp lcd(0x20, I2Cexp_MCP23008, 7, 6, 5, 4, 3, 2, 1, HIGH);
 
 void setup() {
   lcd.begin(16,2);
-  lcd.print("Hi Kimberley");
+
 }
 
 void loop() {
+  lcd.print("Hi Kimberley");
+
+  delay(2000);
+
+  lcd.clear();
+
+  lcd.setCursor(0, 1); // set the position of item sto be printed
+  lcd.print("Hello World");
+
+  delay(1000);
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
 
 }
