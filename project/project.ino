cc93363ae873bc66b5f6f8bd1ed4ba7f1f7182da
kimberley-saka <kimberleysaka@gmail.com>
2024-05-15 12:12:15 +0200

    updated

--- project/project.ino
+++ project/project.ino
@@ -1,10 +1,31 @@
-#define joyStickSwitch 1
-#define analogInputy 2
-#define analogInputX 3
+#define analogInputy 1
+#define analogInputX 2
+
+#define led2 2
+#define led3 3
+#define led4 4 
+#define led5 5
+#define led6 6 
+#define led7 7
+#define led8 8
+#define led9 9
+#define led10 10
+#define led11 11
+
+
 
 
 void setup() {
-pinMode(joyStickSwitch, INPUT);
+pinMode(led2, INPUT);
+pinMode(led3, INPUT);
+pinMode(led4, INPUT);
+pinMode(led5, INPUT);
+pinMode(led6, INPUT);
+pinMode(led7, INPUT);
+pinMode(led8, INPUT);
+pinMode(led9, INPUT);
+pinMode(led10, INPUT);
+pinMode(led11, INPUT);
 
 Serial.begin(9600);
 
@@ -22,8 +43,8 @@ int outputX1 = map(ReadX, 511, 1023, 0, 100);
 
 if (outputY1 < 2 ) {
   int outputY2 = map(ReadY, 511, 0, 0, 100); // handles negative values
-  Serial.print("Neg-to-posY: ");
-  Serial.println(outputY2);
+  digitalWrite();
+  
 
 }
 
@@ -36,8 +57,7 @@ delay(200);
 
 if (outputX1 < 2 ) {
  int outputX2 = map(ReadX, 511, 0, 0, 100);
- Serial.print("Neg-to-posX: ");
- Serial.println(outputX2);
+ digitalWrite()
 }
 
 else {  
